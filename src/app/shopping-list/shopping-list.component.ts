d37e1f1edfe1a5ed04a7208488bc622bb7955df4
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-10-30 14:42:43 +0700

    directve 1

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('tao', 2),
+    new Ingredient('nho', 3)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
